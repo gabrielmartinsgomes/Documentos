38f924ce59c3df8a0265bd369754c4b29762f599
gabrielmartinsgomes <gabrielhenrickmartind3454@gmail.com>
2023-12-05 16:34:39 -0300

    ih

--- Lista de Tarefas/script.js
+++ Lista de Tarefas/script.js
@@ -1,13 +1,26 @@
-let texto = document.querySelector("#descricao")
-let lista = document.querySelector("#tarefas")
-let adicionarTexto = document.querySelector("#adicionar");
-adicionarTexto.addEventListener("click", function(){
-    implementar(texto.nodeValue, lista)
-})
-function implementar(a,  b){
-    let li = document.createElement("li")
-    li.appendChild(document.createTextNode(texto))
-    lista.appendChild(li)
-        
+let botaoadiconar = document.getElementById("adicionar");
+let containertarefa = document.getElementById("containertarefa");
+let campoinput = document.getElementById("camporesultado")
+let botaolimpar = document.getElementById("limpar");
+let botaolimpar2 = document.getElementById("limpar2")
+let tarefacompleta = document.getElementById("containertarefacompleta")
+
+botaoadiconar.addEventListener("click", function(){
+    let paragrafo = document.createElement("p");
+    paragrafo.classList.add('paragrafoestilo');
+    paragrafo.innerText = campoinput.value; 
+    containertarefa.appendChild(paragrafo);
+    campoinput.value = ""
+    paragrafo.addEventListener("click", function(){
+        containertarefa.removeChild(paragrafo);
+        tarefacompleta.appendChild(paragrafo);
+    })
+    botaolimpar.addEventListener("click", function(){
+        containertarefa.removeChild(paragrafo);
+        tarefacompleta.appendChild(paragrafo);
+    })
+    botaolimpar2.addEventListener("click", function(){
+        tarefacompleta.removeChild(paragrafo);
+    })
     
-}
+})
